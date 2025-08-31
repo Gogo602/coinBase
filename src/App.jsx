4e622e0290a8bccf65abe3c1b65cecfc8cde103d
Gogo602 <gogomarosi61@gmail.com>
2025-08-31 19:46:00 +0300

    File: pages/Home; added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import RootLayout from "./layout/RootLayout";
 import UserLayout from "./layout/UserLayout";
+import Home from "./pages/Home";
 
 export default function App() {
     return (
@@ -12,8 +13,9 @@ export default function App() {
                 <Route path="/:userRole/*" element={<UserLayout />} />
                 
                 {/* The root path for the home page or guest landing page */}
-                <Route path="/" element={<RootLayout />} />
-                
+                <Route path="/" element={<RootLayout />} >
+                    <Route index element={<Home />}/>
+                </Route>                
                 {/* A catch-all route for any undefined paths */}
                 <Route path="*" element={<h1 className="flex items-center justify-center h-screen">404: Not Found</h1>} />
             </Routes>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+
+export default function Home(){
+
+    return (
+        <div className="bg-[#1E0338] flex items-center justify-center h-screen w-full">
+            <div className="space-y-10 text-center text-2xl">
+                <h1 className="text-3xl">Welcome to coinBase</h1>
+                <div className="flex items-center jusfify-center space-x-5">
+                    <Link
+                        to='/user'
+                        className="bg-gradient-to-b from-[#FD749B] to-[#281AC8] px-5 py-2 rounded-md"
+                    >
+                        GO TO USER
+                    </Link>
+                    <Link
+                        to='/agent'
+                        className="bg-gradient-to-b from-[#FD749B] to-[#281AC8] px-5 py-2 rounded-md"
+                    >
+                        GOT TO AGENT
+                    </Link>
+                    <Link
+                        to='admin'
+                        className="bg-gradient-to-b from-[#FD749B] to-[#281AC8] px-5 py-2 rounded-md"
+                    >
+                        GO TO ADMIN
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
