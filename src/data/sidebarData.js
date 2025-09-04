4468353c4a8ea7b2720cc1e3ad029ed81cccaf86
TransposeDev <tofexzyno@gmail.com>
2025-09-04 15:45:50 +0100

    add logout: back to home

--- src/data/sidebarData.js
+++ src/data/sidebarData.js
@@ -1,35 +1,50 @@
-import { FaCog, FaEye, FaWallet } from 'react-icons/fa';
+import { FaCog, FaEye, FaWallet } from "react-icons/fa";
 import { HiOutlineChatAlt2 } from "react-icons/hi";
 import { AiOutlineTransaction } from "react-icons/ai";
 import { BiBarChart } from "react-icons/bi";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 export const sidebarData = {
-    user: [
-        { to: '/user', label: 'Overview', icon: FaEye },
-        { to: '/user/trade', label: 'Trade', icon: HiOutlineChatAlt2 },
-        { to: '/user/wallet', label: 'Wallet', icon: HiOutlineChatAlt2 },
-        { to: '/user/transactions', label: 'Transactions', icon: HiOutlineChatAlt2 },
-        { to: '/user/statistics', label: 'Statistics', icon: HiOutlineChatAlt2 },
-        { to: '/user/setting', label: 'Settings', icon: HiOutlineChatAlt2 },
-    ],
-    agent: [
-        { to: '/agent', label: 'Overview', icon: FaEye },
-        { to: '/agent/users', label: 'User', icon: FaEye },
-        { to: '/agent/trade', label: 'Trade', icon: HiOutlineChatAlt2 },
-        { to: '/agent/wallet', label: 'Wallet', icon: FaWallet },
-        { to: '/agent/transactions', label: 'Transactions', icon: AiOutlineTransaction },
-        { to: '/agent/statistics', label: 'Statistics', icon: BiBarChart },
-        { to: '/agent/settings', label: 'Settings', icon: FaCog },
-    ],
-    admin: [
-        { to: '/admin', label: 'Overview', icon: FaEye },
-        { to: '/admin/users', label: 'Users', icon: FaCog },
-        { to: '/admin/agents', label: 'Agent', icon: BiBarChart },
-        { to: '/admin/cards', label: 'Cards', icon: BiBarChart },
-        { to: '/admin/bitcoin-ethereum', label: 'Bitcoin & Ethereum', icon: BiBarChart },
-        { to: '/admin/payments', label: 'Payment', icon: BiBarChart },
-        { to: '/admin/transactions', label: 'Transaction', icon: BiBarChart },
-        { to: '/admin/statistics', label: 'Statistics', icon: BiBarChart },
-        { to: '/admin/logout', label: 'Logout', icon: BiBarChart },
-    ]
+  user: [
+    { to: "/user", label: "Overview", icon: FaEye },
+    { to: "/user/trade", label: "Trade", icon: HiOutlineChatAlt2 },
+    { to: "/user/wallet", label: "Wallet", icon: HiOutlineChatAlt2 },
+    {
+      to: "/user/transactions",
+      label: "Transactions",
+      icon: HiOutlineChatAlt2,
+    },
+    { to: "/user/statistics", label: "Statistics", icon: HiOutlineChatAlt2 },
+    { to: "/user/setting", label: "Settings", icon: HiOutlineChatAlt2 },
+    { to: "/", label: "Logout", icon: FaArrowAltCircleLeft },
+  ],
+  agent: [
+    { to: "/agent", label: "Overview", icon: FaEye },
+    { to: "/agent/users", label: "User", icon: FaEye },
+    { to: "/agent/trade", label: "Trade", icon: HiOutlineChatAlt2 },
+    { to: "/agent/wallet", label: "Wallet", icon: FaWallet },
+    {
+      to: "/agent/transactions",
+      label: "Transactions",
+      icon: AiOutlineTransaction,
+    },
+    { to: "/agent/statistics", label: "Statistics", icon: BiBarChart },
+    { to: "/agent/settings", label: "Settings", icon: FaCog },
+    { to: '/', label: 'Logout', icon: FaArrowAltCircleLeft },
+  ],
+  admin: [
+    { to: "/admin", label: "Overview", icon: FaEye },
+    { to: "/admin/users", label: "Users", icon: FaCog },
+    { to: "/admin/agents", label: "Agent", icon: BiBarChart },
+    { to: "/admin/cards", label: "Cards", icon: BiBarChart },
+    {
+      to: "/admin/bitcoin-ethereum",
+      label: "Bitcoin & Ethereum",
+      icon: BiBarChart,
+    },
+    { to: "/admin/payments", label: "Payment", icon: BiBarChart },
+    { to: "/admin/transactions", label: "Transaction", icon: BiBarChart },
+    { to: "/admin/statistics", label: "Statistics", icon: BiBarChart },
+    { to: "/", label: "Logout", icon: FaArrowAltCircleLeft },
+  ],
 };
