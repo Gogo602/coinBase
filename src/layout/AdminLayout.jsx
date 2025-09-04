3c95c76576247112688fd52bcf81667944326904
TransposeDev <tofexzyno@gmail.com>
2025-09-04 15:54:47 +0100

    add text-white to admin layout

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -4,7 +4,7 @@ export default function AdminLayout() {
     return (
         <div className='flex'>
             <div className='grow h-full lg:h-full  '>
-                <div className="">
+                <div className="text-white">
                     <Outlet />
                 </div>
             </div>
